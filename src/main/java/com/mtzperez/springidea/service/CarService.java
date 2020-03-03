cd13cf44630c7b5aca400385abc7a6b761eeb3d6
David Martinez <david.martinez@nter.es>
2020-03-03 16:46:34 +0100

    remove bug

--- src/main/java/com/mtzperez/springidea/service/CarService.java
+++ src/main/java/com/mtzperez/springidea/service/CarService.java
@@ -21,59 +21,9 @@ public class CarService {
     }
 
     public CarDto createCar(CarDto carDto) {
-        Object i = null;
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
-        if(i == null){
-            i.toString().substring(-10,2);
-        }
        Car car = new Car(carDto.name);
        carRepository.save(car);
        return new CarDto(car.name);
     }
-
 
 }
