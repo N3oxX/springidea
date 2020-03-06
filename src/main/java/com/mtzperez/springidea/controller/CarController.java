1514cdcb358fe1245a2e295f50f07386a8baa9d2
David Martinez <david.martinez@nter.es>
2020-03-06 14:48:20 +0100

    rabbitmq actuator refresh

--- src/main/java/com/mtzperez/springidea/controller/CarController.java
+++ src/main/java/com/mtzperez/springidea/controller/CarController.java
@@ -4,6 +4,7 @@ import com.mtzperez.springidea.model.dto.CarDto;
 import com.mtzperez.springidea.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping("cars")
 public class CarController {
